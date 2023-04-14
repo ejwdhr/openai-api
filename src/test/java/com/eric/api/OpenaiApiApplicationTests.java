27aa653c962590a0a8d581d232092b7b4a59e329
ejwdhr <623622771@qq.co>
2023-04-14 16:56:11 +0800

    Initial commit

---
+++ src/test/java/com/eric/api/OpenaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
